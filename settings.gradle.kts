22843451ee57407f98265685139b469af29a0f1a
MontacerElFazazi <montacer.elfazazielfazazi@cieep.com>
2023-12-18 11:20:31 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ejerc1RetrofitPmdmTema2"
+include(":app")
+ 
